8dfae1fb726727b12da89c1981b5ad444cea8d5b
Louvre Chow <louvrecly@gmail.com>
2020-02-27 00:54:57 +0800

    feat/headnav-and-recordbutton-ui: Create RecordButton component

---
+++ src/Components/RecordButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './RecordButton.css';
+
+const RecordButton: React.FC = () => {
+  return (
+    <div className="record-button">
+      <button className="button">9up</button>
+    </div>
+  );
+}
+
+export default RecordButton;
