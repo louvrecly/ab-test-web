0c7aaf45f9fd00cfea6f9bdce7610c5893d35997
Louvre Chow <louvrecly@gmail.com>
2020-02-27 00:53:09 +0800

    feat/headnav-and-recordbutton-ui: Create HeadNav component

---
+++ src/Components/HeadNav.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './HeadNav.css';
+
+interface IHeadNavProps {
+  title: string
+}
+
+const HeadNav: React.FC<IHeadNavProps> = ({ title }: IHeadNavProps) => {
+  return (
+    <div className="head-nav">
+      <button className="menu nav-button">三</button>
+      <div className="switch">
+        Switch
+      </div>
+      <button className="search nav-button">Q</button>
+    </div>
+  );
+}
+
+export default HeadNav;
