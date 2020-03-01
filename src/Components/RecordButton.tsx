beed9c53bb6b8bebaeb0857c5630e4a91898b055
Louvre Chow <louvrecly@gmail.com>
2020-03-02 02:02:09 +0800

    feat/headnav-and-recordbutton-ui: Convert html elements in RecordButton into material-ui compoonents

--- src/Components/RecordButton.tsx
+++ src/Components/RecordButton.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { IconButton } from '@material-ui/core';
 import './RecordButton.css';
 
 const RecordButton: React.FC = () => {
   return (
     <div className="record-button">
-      <button className="button">9up</button>
+      <IconButton className="button" aria-label="record">
+        9up
+      </IconButton>
     </div>
   );
 };
