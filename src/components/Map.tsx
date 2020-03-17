0e86c68a81dbf5da7dad3101c5b82792553a429a
Louvre Chow <louvrecly@gmail.com>
2020-03-17 20:00:37 +0800

    feat/leaflet-map: Create Map component with useEffect to initiate the map view by leaflet

---
+++ src/components/Map.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import L from 'leaflet';
+
+interface IMapProps {
+  children: React.ReactNode;
+}
+
+const Map: React.FC<IMapProps> = (props: IMapProps) => {
+  useEffect(() => {
+    L.map('map', {
+      center: [22.2988, 114.1722],
+      zoom: 12,
+      zoomSnap: 0.1,
+      zoomDelta: 0.5,
+      layers: [
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          attribution:
+            '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        })
+      ]
+    });
+  }, []);
+
+  return <div id="map">{props.children}</div>;
+};
+
+export default Map;
