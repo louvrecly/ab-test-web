13354a447f27f67f0e7541f90a48008a749da7be
Louvre Chow <louvrecly@gmail.com>
2020-03-23 00:06:36 +0800

    feat/redux-store: Import Main to App, Wrap everything with ConnectedRouter with history provided and Dispatch getThreads before render
    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
 import Map from 'components/Map';
-import HeadNav from 'components/HeadNav';
-import RecordButton from 'components/RecordButton';
+import Main from 'components/Main';
+import { history, IRootState, ThunkResult } from './store';
+import { getThreads } from 'redux/threads/thunks';
 import classes from './App.module.scss';
 
-const App: React.FC = () => {
-  const markersData: [number, number][] = [
-    [22.2988, 114.1742],
-    [22.4423, 114.1655],
-    [22.3119, 114.2569],
-    [22.3837, 114.2708],
-    [22.2885, 114.1928],
-    [22.2801, 114.1588],
-    [22.3908, 113.9725]
-  ];
+interface IAppProps {
+  getThreads: () => void;
+}
+
+const App: React.FC<IAppProps> = (props: IAppProps) => {
+  props.getThreads();
 
   return (
-    <div className={classes.app}>
-      <Map markersData={markersData} />
-      <HeadNav title="吹水台" />
-      <RecordButton />
-    </div>
+    <ConnectedRouter history={history}>
+      <div className={classes.app}>
+        <Map />
+        <Main />
+      </div>
+    </ConnectedRouter>
   );
 };
 
-export default App;
+const mapStateToProps = (state: IRootState) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch: ThunkResult) => {
+  return {
+    getThreads: () => dispatch(getThreads())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
