60631e42a74bdd5c18bd0ca634ec6cc23cac4e1d
Louvre Chow <louvrecly@gmail.com>
2020-03-28 16:39:52 +0800

    feat/redux-store_active-thread: Name tsimShaTsuiLatLng as the initial center of the map
    wip

--- src/components/Map/constant.ts
+++ src/components/Map/constant.ts
@@ -1,13 +1,19 @@
 import L from 'leaflet';
 
+const tsimShaTsuiLatLng: [number, number] = [22.2988, 114.1722];
+
 export const urlTemplate: string =
   'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png';
 
-export const attribution: string =
-  '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+export const attribution: string = `
+  &copy; <a href="https://stadiamaps.com/">Stadia Maps</a>,
+  &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a>
+  &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>
+  contributors
+`;
 
 export const options: L.MapOptions = {
-  center: [22.2988, 114.1722],
+  center: tsimShaTsuiLatLng,
   zoom: 12,
   zoomSnap: 0.1,
   zoomDelta: 0.5,
