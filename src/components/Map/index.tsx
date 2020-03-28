a7d27110f8877fc0eb7790697b1c7f99ad079b4b
Louvre Chow <louvrecly@gmail.com>
2020-03-28 16:35:47 +0800

    feat/redux-store_active-thread: Add setActiveThread dispatch to IMapProps and Dispatch it on popupopen and popupclose events

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -5,49 +5,70 @@ import { Thread } from 'models';
 import { IRootState, ThunkResult } from 'store';
 import { urlTemplate, attribution, options } from './constant';
 import classes from './styles.module.scss';
+import { setActiveThread } from 'redux/threads/actions';
 
 interface IMapProps {
   threads: Array<Thread>;
+  setActiveThread: (thread?: Thread) => void;
 }
 
 const Map: React.FC<IMapProps> = (props: IMapProps) => {
   const mapRef: React.MutableRefObject<L.Map | null> = useRef(null);
   const layerRef: React.MutableRefObject<L.LayerGroup | null> = useRef(null);
+  const markerRef: React.MutableRefObject<L.Marker | null> = useRef(null);
 
+  /* initialize map and add tile layer */
   useEffect(() => {
     const container = L.DomUtil.get('map');
     if (container) {
       (container as any)._leaflet_id = null;
     }
+
     const tileLayer: L.TileLayer = L.tileLayer(urlTemplate, {
       className: classes['tile-layer'],
       attribution
     });
+
     const mapOptions: L.MapOptions = { ...options, layers: [tileLayer] };
     mapRef.current = L.map('map', mapOptions);
   }, []);
 
+  /* create layer group */
   useEffect(() => {
     layerRef.current = L.layerGroup().addTo(mapRef.current as L.Map);
   }, []);
 
+  /* create popup and markers */
   useEffect(() => {
     (layerRef.current as L.LayerGroup).clearLayers();
     const icon: L.DivIcon = L.divIcon({ className: classes.icon });
 
-    props.threads.forEach(({ location }) => {
+    props.threads.forEach(thread => {
       const position = L.latLng([
-        (location as any)._latitude,
-        (location as any)._longitude
+        (thread.location as any)._latitude,
+        (thread.location as any)._longitude
       ]);
 
+      const popup: L.Popup = L.popup({
+        className: classes.popup,
+        closeButton: false,
+        pane: 'shadowPane' /* avoid masking other markers */
+      });
+
       L.marker(position, { icon })
-        .on('click', () => {
+        .bindPopup(popup)
+        .on('popupopen', (event: L.LeafletEvent) => {
+          markerRef.current = event.target as L.Marker;
+          props.setActiveThread(thread);
           (mapRef.current as L.Map).flyTo(position);
+        })
+        .on('popupclose', () => {
+          markerRef.current = null;
+          props.setActiveThread();
         })
         .addTo(layerRef.current as L.LayerGroup);
     });
-  }, [props.threads]);
+  }, [props]);
 
   return <div id="map" className={classes.map} />;
 };
@@ -59,7 +80,9 @@ const mapStateToProps = (state: IRootState) => {
 };
 
 const mapDispatchToProps = (dispatch: ThunkResult) => {
-  return {};
+  return {
+    setActiveThread: (thread?: Thread) => dispatch(setActiveThread(thread))
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Map);
