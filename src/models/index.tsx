4d06a39a7f8cbb27e86795c544ef9429e9d34a41
Louvre Chow <louvrecly@gmail.com>
2020-04-01 00:58:36 +0800

    feat/redux-store_users: Add User interface to models

--- src/models/index.tsx
+++ src/models/index.tsx
@@ -11,3 +11,18 @@ export interface Thread {
   timestamp: firestore.Timestamp;
   bookmarked_by_users: Array<string>;
 }
+
+export interface User {
+  id?: string;
+  username: string;
+  email: string;
+  type: 'admin' | 'member';
+  is_active?: boolean;
+  self_voice_url?: string;
+  bookmarked_voices?: Array<string>;
+  history?: Array<string>;
+  bookmarked_threads?: Array<string>;
+  followed_members?: Array<string>;
+  followers?: Array<string>;
+  blocked_members?: Array<string>;
+}
