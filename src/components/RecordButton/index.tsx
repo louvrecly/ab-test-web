91520a2c708d168079a18acb4f83e40666c8d130
Louvre Chow <louvrecly@gmail.com>
2020-04-16 22:31:38 +0800

    feat/redux-store_voices: Add isAbsolute prop to IRecordButtonProps for conditional styling

--- src/components/RecordButton/index.tsx
+++ src/components/RecordButton/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import clsx from 'clsx';
 import { IconButton } from '@material-ui/core';
 import classes from './styles.module.scss';
 
-const RecordButton: React.FC = () => {
+interface IRecordButtonProps {
+  isAbsolute?: boolean;
+}
+
+const RecordButton: React.FC<IRecordButtonProps> = (
+  props: IRecordButtonProps
+) => {
   return (
-    <div className={classes['record-button']}>
+    <div
+      className={clsx({
+        [classes['record-button']]: true,
+        [classes.absolute]: props.isAbsolute
+      })}
+    >
       <IconButton className={classes.button} aria-label="record">
         9up
       </IconButton>
