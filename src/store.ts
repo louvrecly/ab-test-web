1167e7f3fe3c6de7bc2f170bece001636888c833
Louvre Chow <louvrecly@gmail.com>
2020-05-08 22:35:30 +0800

    feat/audio-recorder: Import IAudioState, IAudioAction and audioReducer into redux store

--- src/store.ts
+++ src/store.ts
@@ -16,14 +16,17 @@ import logger from 'redux-logger';
 import { IThreadsState } from 'redux/threads/state';
 import { IVoicesState } from 'redux/voices/state';
 import { IUsersState } from 'redux/users/state';
+import { IAudiosState } from 'redux/audios/state';
 import { IComponentsState } from 'redux/components/state';
 import { IThreadsAction } from 'redux/threads/actions';
 import { IVoicesAction } from 'redux/voices/actions';
 import { IUsersAction } from 'redux/users/actions';
+import { IAudiosAction } from 'redux/audios/actions';
 import { IComponentsAction } from 'redux/components/actions';
 import { threadsReducer } from 'redux/threads/reducer';
 import { voicesReducer } from 'redux/voices/reducer';
 import { usersReducer } from 'redux/users/reducer';
+import { audiosReducer } from 'redux/audios/reducer';
 import { componentsReducer } from 'redux/components/reducer';
 
 declare global {
@@ -41,6 +44,7 @@ export interface IRootState {
   threads: IThreadsState;
   voices: IVoicesState;
   users: IUsersState;
+  audios: IAudiosState;
   components: IComponentsState;
   router: RouterState;
 }
@@ -49,12 +53,14 @@ type IRootAction =
   | IThreadsAction
   | IVoicesAction
   | IUsersAction
+  | IAudiosAction
   | IComponentsAction;
 
 const rootReducer = combineReducers<IRootState>({
   threads: threadsReducer,
   voices: voicesReducer,
   users: usersReducer,
+  audios: audiosReducer,
   components: componentsReducer,
   router: connectRouter(history)
 });
