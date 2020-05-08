a5208397968613f4d2eac9f65fdef6aba5915d24
Louvre Chow <louvrecly@gmail.com>
2020-05-08 22:41:48 +0800

    feat/audio-recorder: Create TimerBar component

---
+++ src/components/TimerBar/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './styles.module.scss';
+
+const TimerBar: React.FC = () => {
+  return <div className={classes['timer-bar']}>00:00</div>;
+};
+
+export default TimerBar;
