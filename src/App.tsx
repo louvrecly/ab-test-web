95ea845da8bd36b43465ef19c1408b58b7ad38b2
Louvre Chow <louvrecly@gmail.com>
2020-05-08 22:43:28 +0800

    feat/audio-recorder: Add initializeRecorder on rendering the App

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,26 @@ import Main from 'components/Main';
 import { history, IRootState, ThunkResult } from './store';
 import { loadThreads } from 'redux/threads/thunks';
 import { loadUsers } from 'redux/users/thunks';
+import { audioRecorder, AudioRecorder } from 'utils/audioRecorder';
 import classes from './App.module.scss';
+import { setRecorder } from 'redux/audios/actions';
 
 interface IAppProps {
   loadThreads: () => void;
   loadUsers: () => void;
+  setRecorder: (recorder?: AudioRecorder) => void;
 }
 
 const App: React.FC<IAppProps> = (props: IAppProps) => {
+  const initializeRecorder = async () => {
+    const recorder = await audioRecorder();
+    props.setRecorder(recorder);
+  };
+
   props.loadThreads();
   props.loadUsers();
+
+  initializeRecorder();
 
   return (
     <ConnectedRouter history={history}>
@@ -34,7 +44,8 @@ const mapStateToProps = (state: IRootState) => {
 const mapDispatchToProps = (dispatch: ThunkResult) => {
   return {
     loadThreads: () => dispatch(loadThreads()),
-    loadUsers: () => dispatch(loadUsers())
+    loadUsers: () => dispatch(loadUsers()),
+    setRecorder: (recorder?: AudioRecorder) => dispatch(setRecorder(recorder))
   };
 };
 
