a5b890c2659215753db48ca4b30f4ca909a0705c
Louvre Chow <louvrecly@gmail.com>
2020-05-10 23:58:11 +0800

    feat/audio-recorder: Add setShowRecordButtonState and setShowPlayListState props to Map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useRef } from 'react';
-import { connect } from 'react-redux';
 import L from 'leaflet';
 import { Thread } from 'models';
 import { IRootState, ThunkResult } from 'store';
 import { DrawerSide } from 'redux/components/state';
 import { setActiveThread, stopPlayingThread } from 'redux/threads/actions';
+import {
+  setDrawerState,
+  setShowRecordButtonState,
+  setShowPlayListState
+} from 'redux/components/actions';
 import { loadVoices } from 'redux/voices/thunks';
-import { setDrawerState } from 'redux/components/actions';
+import { connect } from 'react-redux';
 import { urlTemplate, attribution, options } from './constant';
 import classes from './styles.module.scss';
 
@@ -16,6 +20,8 @@ interface IMapProps {
   loadVoices: (threadId: string) => void;
   stopPlayingThread: () => void;
   setDrawerState: (side: DrawerSide, open: boolean) => void;
+  setShowRecordButtonState: (showRecordButton: boolean) => void;
+  setShowPlayListState: (showPlayList: boolean) => void;
 }
 
 const Map: React.FC<IMapProps> = ({
@@ -23,7 +29,9 @@ const Map: React.FC<IMapProps> = ({
   setActiveThread, /* tslint:disable-line */
   loadVoices, /* tslint:disable-line */
   stopPlayingThread, /* tslint:disable-line */
-  setDrawerState /* tslint:disable-line */
+  setDrawerState, /* tslint:disable-line */
+  setShowRecordButtonState, /* tslint:disable-line */
+  setShowPlayListState /* tslint:disable-line */
 }: IMapProps) => {
   const mapRef: React.MutableRefObject<L.Map | null> = useRef(null);
   const layerRef: React.MutableRefObject<L.LayerGroup | null> = useRef(null);
@@ -67,24 +75,39 @@ const Map: React.FC<IMapProps> = ({
         pane: 'shadowPane' /* avoid masking other markers */
       });
 
+      const popupOpenHandler = (event: L.LeafletEvent) => {
+        markerRef.current = event.target as L.Marker;
+        stopPlayingThread();
+        setActiveThread(thread);
+        loadVoices(thread.id as string);
+        (mapRef.current as L.Map).flyTo(position);
+        setShowPlayListState(false);
+        setShowRecordButtonState(false);
+        setDrawerState('bottom', true);
+      };
+
+      const popupCloseHandler = () => {
+        markerRef.current = null;
+        setActiveThread();
+        setShowRecordButtonState(true);
+        setDrawerState('bottom', false);
+      };
+
       L.marker(position, { icon })
         .bindPopup(popup)
-        .on('popupopen', (event: L.LeafletEvent) => {
-          markerRef.current = event.target as L.Marker;
-          stopPlayingThread();
-          setActiveThread(thread);
-          loadVoices(thread.id as string);
-          (mapRef.current as L.Map).flyTo(position);
-          setDrawerState('bottom', true);
-        })
-        .on('popupclose', () => {
-          markerRef.current = null;
-          setActiveThread();
-          setDrawerState('bottom', false);
-        })
+        .on('popupopen', popupOpenHandler)
+        .on('popupclose', popupCloseHandler)
         .addTo(layerRef.current as L.LayerGroup);
     });
-  }, [threads, stopPlayingThread, setActiveThread, loadVoices, setDrawerState]);
+  }, [
+    threads,
+    stopPlayingThread,
+    setActiveThread,
+    loadVoices,
+    setDrawerState,
+    setShowRecordButtonState,
+    setShowPlayListState
+  ]);
 
   return <div id="map" className={classes.map} />;
 };
@@ -102,7 +125,11 @@ const mapDispatchToProps = (dispatch: ThunkResult) => {
     loadVoices: (threadId: string) => dispatch(loadVoices(threadId)),
     stopPlayingThread: () => dispatch(stopPlayingThread()),
     setDrawerState: (side: DrawerSide, open: boolean) =>
-      dispatch(setDrawerState(side, open))
+      dispatch(setDrawerState(side, open)),
+    setShowRecordButtonState: (showRecordButton: boolean) =>
+      dispatch(setShowRecordButtonState(showRecordButton)),
+    setShowPlayListState: (showPlayList: boolean) =>
+      dispatch(setShowPlayListState(showPlayList))
   };
 };
 
