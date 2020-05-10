a560a70f6a4728eb4c7796dc37387b954fcf3b56
Louvre Chow <louvrecly@gmail.com>
2020-05-10 23:57:08 +0800

    feat/audio-recorder: Remove setDrawerState prop and Add showRecordButton and showPlayList props for the display logic of RecordButton in Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -4,58 +4,35 @@ import RecordButton from 'components/RecordButton';
 import DrawerContainer from 'components/DrawerContainer';
 import ThreadPanel from 'components/ThreadPanel';
 import TimerBar from 'components/TimerBar';
-import { drawerSides } from 'components/DrawerContainer/constant';
 import { Thread } from 'models';
 import { IRootState, ThunkResult } from 'store';
-import { DrawerState, DrawerSide } from 'redux/components/state';
-import { setDrawerState } from 'redux/components/actions';
+import { DrawerState } from 'redux/components/state';
 import { connect } from 'react-redux';
 import classes from './styles.module.scss';
 
 interface IMainProps {
   drawerState: DrawerState;
   activeThread: Thread | undefined;
+  showRecordButton: boolean;
+  showPlayList: boolean;
   isRecording: boolean;
-  setDrawerState: (side: DrawerSide, open: boolean) => void;
 }
 
 const Main: React.FC<IMainProps> = (props: IMainProps) => {
-  const toggleDrawer = (side: DrawerSide, open: boolean) => (
-    event: React.KeyboardEvent | React.MouseEvent
-  ) => {
-    if (
-      event &&
-      event.type === 'keydown' &&
-      ((event as React.KeyboardEvent).key === 'Tab' ||
-        (event as React.KeyboardEvent).key === 'Shift')
-    ) {
-      return;
-    }
-    props.setDrawerState(side, open);
-  };
-
   return (
     <div className={classes.main}>
-      <HeadNav title="吹水台" toggleDrawer={toggleDrawer} />
+      <HeadNav title="吹水台" />
 
-      {drawerSides.reduce((combinedState: boolean, drawerSide: DrawerSide) => {
-        return combinedState || props.drawerState[drawerSide];
-      }, false) ? null : (
+      {(props.showRecordButton || props.showPlayList) && (
         <RecordButton isAbsolute={true} />
       )}
 
-      <DrawerContainer
-        side="left"
-        open={props.drawerState.left}
-        toggleDrawer={toggleDrawer}
-      >
+      <DrawerContainer side="left">
         <p>drawer contents</p>
       </DrawerContainer>
 
       <DrawerContainer
         side="bottom"
-        open={props.drawerState.bottom}
-        toggleDrawer={toggleDrawer}
         disableSwipe={props.activeThread === undefined}
       >
         <ThreadPanel />
@@ -70,15 +47,14 @@ const mapStateToProps = (state: IRootState) => {
   return {
     drawerState: state.components.drawerState,
     activeThread: state.threads.activeThread,
+    showRecordButton: state.components.showRecordButton,
+    showPlayList: state.components.showPlayList,
     isRecording: state.audios.isRecording
   };
 };
 
 const mapDispatchToProps = (dispatch: ThunkResult) => {
-  return {
-    setDrawerState: (side: DrawerSide, open: boolean) =>
-      dispatch(setDrawerState(side, open))
-  };
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
