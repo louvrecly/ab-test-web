3de48cc489cf08f7e37c5109e896887bb69695e1
Louvre Chow <louvrecly@gmail.com>
2020-05-10 23:33:32 +0800

    feat/audio-recorder: Add setters for showRecordButton and showPlayList to IComponentsAction

--- src/redux/components/actions.ts
+++ src/redux/components/actions.ts
@@ -1,5 +1,19 @@
 import { DrawerSide } from 'redux/components/state';
 
+export function setShowRecordButtonState(showRecordButton: boolean) {
+  return {
+    type: 'SET_SHOW_RECORD_BUTTON_STATE' as 'SET_SHOW_RECORD_BUTTON_STATE',
+    showRecordButton
+  };
+}
+
+export function setShowPlayListState(showPlayList: boolean) {
+  return {
+    type: 'SET_SHOW_PLAY_LIST_STATE' as 'SET_SHOW_PLAY_LIST_STATE',
+    showPlayList
+  };
+}
+
 export function setDrawerState(side: DrawerSide, open: boolean) {
   return {
     type: 'SET_DRAWER_STATE' as 'SET_DRAWER_STATE',
@@ -8,6 +22,9 @@ export function setDrawerState(side: DrawerSide, open: boolean) {
   };
 }
 
-type ComponentsActionCreators = typeof setDrawerState;
+type ComponentsActionCreators =
+  | typeof setShowRecordButtonState
+  | typeof setShowPlayListState
+  | typeof setDrawerState;
 
 export type IComponentsAction = ReturnType<ComponentsActionCreators>;
