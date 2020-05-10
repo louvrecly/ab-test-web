4ef26a6ea8d242529386d266a785062041c842b0
Louvre Chow <louvrecly@gmail.com>
2020-05-10 23:35:31 +0800

    feat/audio-recorder: Add SET_SHOW_RECORD_BUTTON_STATE and SET_SHOW_PLAY_LIST_STATE cases to componentsReducer

--- src/redux/components/reducer.ts
+++ src/redux/components/reducer.ts
@@ -2,6 +2,8 @@ import { IComponentsState } from 'redux/components/state';
 import { IComponentsAction } from 'redux/components/actions';
 
 const initialState: IComponentsState = {
+  showRecordButton: true,
+  showPlayList: false,
   drawerState: {
     top: false,
     right: false,
@@ -15,6 +17,18 @@ export function componentsReducer(
   action: IComponentsAction
 ) {
   switch (action.type) {
+    case 'SET_SHOW_RECORD_BUTTON_STATE':
+      const { showRecordButton } = action;
+      return {
+        ...state,
+        showRecordButton
+      };
+    case 'SET_SHOW_PLAY_LIST_STATE':
+      const { showPlayList } = action;
+      return {
+        ...state,
+        showPlayList
+      };
     case 'SET_DRAWER_STATE':
       const { side, open } = action;
       const { drawerState } = state;
