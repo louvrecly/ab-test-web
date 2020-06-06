da7c5d00a5cffcd83aca5fa2261b06957277b9e6
Louvre Chow <louvrecly@gmail.com>
2020-06-06 15:16:17 +0800

    feat/audio-recorder_ui: Comment out audioRecorder utils for pure UI development first

--- src/components/RecordButton/index.tsx
+++ src/components/RecordButton/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import clsx from 'clsx';
 import { IconButton } from '@material-ui/core';
-import { AudioData, AudioRecorder } from 'utils/audioRecorder';
+// import { AudioData, AudioRecorder } from 'utils/audioRecorder';
 import { IRootState, ThunkResult } from 'store';
-import { setAudio, setIsRecordingState } from 'redux/audios/actions';
+// import { setAudio, setIsRecordingState } from 'redux/audios/actions';
+import { setIsRecordingState } from 'redux/audios/actions';
 import { connect } from 'react-redux';
 import classes from './styles.module.scss';
 
 interface IRecordButtonProps {
-  recorder: AudioRecorder | undefined;
+  // recorder: AudioRecorder | undefined;
   isAbsolute?: boolean;
   isRecording: boolean;
-  setAudio: (audio: AudioData) => void;
+  // setAudio: (audio: AudioData) => void;
   setIsRecordingState: (isRecording: boolean) => void;
 }
 
@@ -19,18 +20,19 @@ const RecordButton: React.FC<IRecordButtonProps> = (
   props: IRecordButtonProps
 ) => {
   const startRecording = () => {
-    if (props.recorder) {
-      props.recorder.start();
-      props.setIsRecordingState(true);
-    } else {
-      console.log('recorder is not initialized!'); // tslint:disable-line
-    }
+    props.setIsRecordingState(true);
+    // if (props.recorder) {
+    //   props.recorder.start();
+    //   props.setIsRecordingState(true);
+    // } else {
+    //   console.log('recorder is not initialized!'); // tslint:disable-line
+    // }
   };
 
   const stopRecording = async () => {
     if (props.isRecording) {
-      const audio = await props.recorder?.stop();
-      props.setAudio(audio as AudioData);
+      // const audio = await props.recorder?.stop();
+      // props.setAudio(audio as AudioData);
       props.setIsRecordingState(false);
     } else {
       console.log('no audio is being recorded'); // tslint:disable-line
@@ -58,14 +60,14 @@ const RecordButton: React.FC<IRecordButtonProps> = (
 
 const mapStateToProps = (state: IRootState) => {
   return {
-    recorder: state.audios.recorder,
+    // recorder: state.audios.recorder,
     isRecording: state.audios.isRecording
   };
 };
 
 const mapDispatchToProps = (dispatch: ThunkResult) => {
   return {
-    setAudio: (audio: AudioData) => dispatch(setAudio(audio)),
+    // setAudio: (audio: AudioData) => dispatch(setAudio(audio)),
     setIsRecordingState: (isRecording: boolean) =>
       dispatch(setIsRecordingState(isRecording))
   };
--- src/redux/audios/state.ts
+++ src/redux/audios/state.ts
@@ -1,7 +1,7 @@
-import { AudioRecorder, AudioData } from 'utils/audioRecorder';
+// import { AudioRecorder, AudioData } from 'utils/audioRecorder';
 
 export interface IAudiosState {
-  recorder: AudioRecorder | undefined;
-  audio: AudioData | undefined;
+  // recorder: AudioRecorder | undefined;
+  // audio: AudioData | undefined;
   isRecording: boolean;
 }
