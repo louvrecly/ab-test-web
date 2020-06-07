3d2685a54df2f2db8e920b6dd3525a68336c858d
Louvre Chow <louvrecly@gmail.com>
2020-06-07 16:55:37 +0800

    feat/audio-recorder_ui: Create VoiceForm

---
+++ src/components/VoiceForm/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './styles.module.scss';
+
+const VoiceForm: React.FC = () => {
+  return <div className={classes['voice-form']}>VoiceForm</div>;
+};
+
+export default VoiceForm;
