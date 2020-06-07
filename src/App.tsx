da96187988926e01b0f7f58518526ef18e654fb2
Louvre Chow <louvrecly@gmail.com>
2020-06-07 16:52:58 +0800

    feat/audio-recorder_ui: Extract ConnectedRouter from the App to encapsulate the App in index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router';
 import Map from 'components/Map';
 import Main from 'components/Main';
-import { history, IRootState, ThunkResult } from './store';
+import { IRootState, ThunkResult } from './store';
 import { loadThreads } from 'redux/threads/thunks';
 import { loadUsers } from 'redux/users/thunks';
 // import { audioRecorder, AudioRecorder } from 'utils/audioRecorder';
@@ -29,12 +28,10 @@ const App: React.FC<IAppProps> = (props: IAppProps) => {
   initializeRecorder();
 
   return (
-    <ConnectedRouter history={history}>
-      <div className={classes.app}>
-        <Map />
-        <Main />
-      </div>
-    </ConnectedRouter>
+    <div className={classes.app}>
+      <Map />
+      <Main />
+    </div>
   );
 };
 
