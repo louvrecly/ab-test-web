fb151060248bf0b576a4e74ae59a0a1cb857a924
Louvre Chow <louvrecly@gmail.com>
2020-06-15 22:32:37 +0800

    feat/audio-recorder_ui: Combine ThreadForm and VoiceForm and Add an optional prop thread to it

--- src/components/VoiceForm/index.tsx
+++ src/components/VoiceForm/index.tsx
@@ -1,8 +1,72 @@
 import React from 'react';
+import { IconButton } from '@material-ui/core';
+import VoicePlayer from 'components/VoicePlayer';
+import { RiFileAddLine } from 'react-icons/ri';
+import { FaMusic, FaCheck } from 'react-icons/fa';
+import { Thread } from 'models';
 import classes from './styles.module.scss';
 
-const VoiceForm: React.FC = () => {
-  return <div className={classes['voice-form']}>VoiceForm</div>;
+interface IVoiceFormProps {
+  thread?: Thread;
+}
+
+const VoiceForm: React.FC<IVoiceFormProps> = (props: IVoiceFormProps) => {
+  return (
+    <div className={classes['voice-form']}>
+      <div className={classes.container}>
+        <div className={classes.handle} />
+
+        <form className={classes.form}>
+          <ul className={classes.fields}>
+            {props.thread && (
+              <li className={classes.field}>
+                <label className={classes.label} htmlFor="channel">
+                  選擇版區：
+                </label>
+
+                <select className={classes.select} name="channel">
+                  <option className={classes.option} value="吹水台">
+                    吹水台
+                  </option>
+                </select>
+              </li>
+            )}
+
+            <li className={classes.field}>
+              <label className={classes.label} htmlFor="thread">
+                新貼題目：
+              </label>
+
+              <input
+                className={classes.input}
+                name="thread"
+                type="text"
+                value={props.thread?.title}
+              />
+            </li>
+          </ul>
+
+          <VoicePlayer />
+
+          <div className={classes.control}>
+            <div className={classes.buttons}>
+              <IconButton className={classes.new} aria-label="new">
+                <RiFileAddLine />
+              </IconButton>
+
+              <IconButton className={classes.music} aria-label="music">
+                <FaMusic />
+              </IconButton>
+            </div>
+
+            <IconButton className={classes.submit} aria-label="submit">
+              <FaCheck />
+            </IconButton>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default VoiceForm;
