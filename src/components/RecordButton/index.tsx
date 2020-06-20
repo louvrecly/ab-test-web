55cfaed5f887329deaa29ca49c5479c4d7546ffa
Louvre Chow <louvrecly@gmail.com>
2020-06-20 23:21:51 +0800

    feat/audio-recorder_recording: Uncomment audioRecorder utils and logic

--- src/components/RecordButton/index.tsx
+++ src/components/RecordButton/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { IconButton } from '@material-ui/core';
-// import { AudioData, AudioRecorder } from 'utils/audioRecorder';
+import { AudioData, AudioRecorder } from 'utils/audioRecorder';
 import { DrawerSide } from 'redux/components/state';
 import { IRootState, ThunkResult } from 'store';
-// import { setAudio, setIsRecordingState } from 'redux/audios/actions';
-import { setIsRecordingState } from 'redux/audios/actions';
+import { setAudio, setIsRecordingState } from 'redux/audios/actions';
 import { setDrawerState } from 'redux/components/actions';
 import { connect } from 'react-redux';
 import classes from './styles.module.scss';
 import { useHistory } from 'react-router-dom';
 
 interface IRecordButtonProps {
-  // recorder: AudioRecorder | undefined;
+  recorder: AudioRecorder | undefined;
   isRecording: boolean;
-  // setAudio: (audio: AudioData) => void;
+  setAudio: (audio: AudioData) => void;
   setIsRecordingState: (isRecording: boolean) => void;
   setDrawerState: (side: DrawerSide, open: boolean) => void;
 }
@@ -24,20 +23,19 @@ const RecordButton: React.FC<IRecordButtonProps> = (
   const history = useHistory();
 
   const startRecording = () => {
-    props.setDrawerState('bottom', true);
-    props.setIsRecordingState(true);
-    // if (props.recorder) {
-    //   props.recorder.start();
-    //   props.setIsRecordingState(true);
-    // } else {
-    //   console.log('recorder is not initialized!'); // tslint:disable-line
-    // }
+    if (props.recorder) {
+      props.setDrawerState('bottom', true);
+      props.recorder.start();
+      props.setIsRecordingState(true);
+    } else {
+      console.log('recorder is not initialized!'); // tslint:disable-line
+    }
   };
 
   const stopRecording = async () => {
     if (props.isRecording) {
-      // const audio = await props.recorder?.stop();
-      // props.setAudio(audio as AudioData);
+      const audio = await props.recorder?.stop();
+      props.setAudio(audio as AudioData);
       props.setIsRecordingState(false);
       history.push('/threads/new');
     } else {
@@ -61,14 +59,14 @@ const RecordButton: React.FC<IRecordButtonProps> = (
 
 const mapStateToProps = (state: IRootState) => {
   return {
-    // recorder: state.audios.recorder,
+    recorder: state.audios.recorder,
     isRecording: state.audios.isRecording
   };
 };
 
 const mapDispatchToProps = (dispatch: ThunkResult) => {
   return {
-    // setAudio: (audio: AudioData) => dispatch(setAudio(audio)),
+    setAudio: (audio: AudioData) => dispatch(setAudio(audio)),
     setIsRecordingState: (isRecording: boolean) =>
       dispatch(setIsRecordingState(isRecording)),
     setDrawerState: (side: DrawerSide, open: boolean) =>
