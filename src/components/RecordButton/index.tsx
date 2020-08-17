8b18775628d5306c60b15b2ae16d2170185d13b7
Louvre Chow <louvrecly@gmail.com>
2020-08-18 00:06:43 +0800

    feat/audio-recorder_audio-form: Refactor RecordButton to show normal or embedded style

--- src/components/RecordButton/index.tsx
+++ src/components/RecordButton/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import clsx from 'clsx';
 import { IconButton } from '@material-ui/core';
 import { Thread } from 'models';
 import { DrawerSide } from 'redux/components/state';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import { IRootState, ThunkResult } from 'store';
 import { setAudio, setIsRecordingState } from 'redux/audios/actions';
 import {
   setDrawerState,
-  setShowRecordButtonState
+  setShowRecordButtonState,
+  embedRecordButton
 } from 'redux/components/actions';
 import { connect } from 'react-redux';
 import { AudioData, AudioRecorder } from 'utils/audioRecorder';
@@ -17,24 +19,29 @@ interface IRecordButtonProps {
   recorder: AudioRecorder | undefined;
   isRecording: boolean;
   activeThread: Thread | undefined;
-  setAudio: (audio: AudioData) => void;
+  showRecordButton: boolean;
+  embeddedRecordButton: boolean;
+  setAudio: (audio?: AudioData) => void;
   setIsRecordingState: (isRecording: boolean) => void;
   setDrawerState: (side: DrawerSide, open: boolean) => void;
   setShowRecordButtonState: (showRecordButton: boolean) => void;
+  embedRecordButton: (embeddedRecordButton: boolean) => void;
 }
 
 const RecordButton: React.FC<IRecordButtonProps> = (
   props: IRecordButtonProps
 ) => {
   const history = useHistory();
+  const location = useLocation();
 
   const startRecording = () => {
     if (props.recorder) {
       props.setDrawerState('bottom', true);
       props.recorder.start();
       props.setIsRecordingState(true);
+      props.embedRecordButton(false);
     } else {
-      console.log('recorder is not initialized!'); // tslint:disable-line
+      console.log('recorder is not initialized!'); /* tslint:disable-line */
     }
   };
 
@@ -43,25 +50,29 @@ const RecordButton: React.FC<IRecordButtonProps> = (
       const audio = await props.recorder?.stop();
       props.setAudio(audio as AudioData);
       props.setIsRecordingState(false);
-      const pathname = `/threads${
-        props.activeThread ? `/${props.activeThread.id}` : ''
-      }/new`;
+      const pathname = `${location.pathname}/new`;
       history.push(pathname);
       props.setShowRecordButtonState(false);
     } else {
-      console.log('no audio is being recorded'); // tslint:disable-line
+      console.log('no audio is being recorded'); /* tslint:disable-line */
     }
   };
 
   return (
-    <div className={classes['record-button']}>
+    <div
+      className={clsx({
+        [classes['record-button']]: true,
+        [classes.floating]: !props.embeddedRecordButton
+      })}
+    >
       <IconButton
+        id="record"
         className={classes.button}
         aria-label="record"
         onMouseDown={startRecording}
         onMouseUp={stopRecording}
       >
-        9up
+        {props.embeddedRecordButton ? '開始錄' : '9up'}
       </IconButton>
     </div>
   );
@@ -71,19 +82,23 @@ const mapStateToProps = (state: IRootState) => {
   return {
     recorder: state.audios.recorder,
     isRecording: state.audios.isRecording,
-    activeThread: state.threads.activeThread
+    activeThread: state.threads.activeThread,
+    showRecordButton: state.components.showRecordButton,
+    embeddedRecordButton: state.components.embeddedRecordButton
   };
 };
 
 const mapDispatchToProps = (dispatch: ThunkResult) => {
   return {
-    setAudio: (audio: AudioData) => dispatch(setAudio(audio)),
+    setAudio: (audio?: AudioData) => dispatch(setAudio(audio)),
     setIsRecordingState: (isRecording: boolean) =>
       dispatch(setIsRecordingState(isRecording)),
     setDrawerState: (side: DrawerSide, open: boolean) =>
       dispatch(setDrawerState(side, open)),
     setShowRecordButtonState: (showRecordButton: boolean) =>
-      dispatch(setShowRecordButtonState(showRecordButton))
+      dispatch(setShowRecordButtonState(showRecordButton)),
+    embedRecordButton: (embeddedRecordButton: boolean) =>
+      dispatch(embedRecordButton(embeddedRecordButton))
   };
 };
 
