c1289e21efdaa98a53b27435c058e30581153c2a
Louvre Chow <louvrecly@gmail.com>
2020-09-10 22:06:49 +0800

    feat/audio-recorder_post-thread-and-voice: Add createVoice method in Voice thunk

--- src/redux/voices/thunks.ts
+++ src/redux/voices/thunks.ts
@@ -1,5 +1,6 @@
 import { Dispatch } from 'redux';
 import { IVoicesAction, loadVoicesSuccess, failed } from './actions';
+import { VoiceJson } from 'models';
 
 const { REACT_APP_API_SERVER } = process.env;
 
@@ -15,6 +16,25 @@ export function loadVoices(threadId: string) {
       dispatch(loadVoicesSuccess(data));
     } else {
       dispatch(failed('LOAD_VOICES_FAILED', data));
+    }
+  };
+}
+
+export function createVoice(newVoice: VoiceJson) {
+  return async (dispatch: Dispatch<IVoicesAction>) => {
+    const res = await fetch(`${REACT_APP_API_SERVER}/voices`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify(newVoice)
+    });
+    const { isSuccess, data } = await res.json();
+
+    if (isSuccess) {
+      return data as VoiceJson;
+    } else {
+      dispatch(failed('CREATE_VOICE_FAILED', data));
     }
   };
 }
