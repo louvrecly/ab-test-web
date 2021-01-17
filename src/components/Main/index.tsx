c1e6ba16d84b14e6dbf27903523c47385c0df39f
Louvre Chow <louvrecly@gmail.com>
2021-01-17 15:26:24 +0800

    enh/rearrange-components-structure: Move Map into Main and Move loadThreads, loadUsers and setRecorders props into Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import Map from 'components/Map';
 import HeadNav from 'components/HeadNav';
 import RecordButton from 'components/RecordButton';
 import DrawerContainer from 'components/Drawer/Container';
 import BottomDrawerContents from 'components/Drawer/Contents/Bottom';
-import { ThreadJson } from 'models';
 import { IRootState, ThunkResult } from 'store';
 import { DrawerState } from 'redux/components/state';
-import { connect } from 'react-redux';
+import { loadThreads } from 'redux/threads/thunks';
+import { loadUsers } from 'redux/users/thunks';
+import { setRecorder } from 'redux/audios/actions';
+import { ThreadJson } from 'models';
+import { audioRecorder, AudioRecorder } from 'utils/audioRecorder';
 import classes from './styles.module.scss';
 
 interface IMainProps {
@@ -14,11 +19,30 @@ interface IMainProps {
   activeThread: ThreadJson | null;
   showRecordButton: boolean;
   isRecording: boolean;
+  loadThreads: () => void;
+  loadUsers: () => void;
+  setRecorder: (recorder: AudioRecorder | null) => void;
 }
 
 const Main: React.FC<IMainProps> = (props: IMainProps) => {
+  /* define function to initialize recorder */
+  const initializeRecorder = async () => {
+    const recorder = await audioRecorder();
+    props.setRecorder(recorder);
+  };
+
+  /* initialize recorder, load threads and users on start */
+  useEffect(() => {
+    initializeRecorder();
+
+    props.loadThreads();
+    props.loadUsers();
+  }, []); /* eslint-disable-line react-hooks/exhaustive-deps */
+
   return (
     <div className={classes.main}>
+      <Map />
+
       <HeadNav />
 
       {props.showRecordButton && <RecordButton />}
@@ -47,7 +71,12 @@ const mapStateToProps = (state: IRootState) => {
 };
 
 const mapDispatchToProps = (dispatch: ThunkResult) => {
-  return {};
+  return {
+    loadThreads: () => dispatch(loadThreads()),
+    loadUsers: () => dispatch(loadUsers()),
+    setRecorder: (recorder: AudioRecorder | null) =>
+      dispatch(setRecorder(recorder))
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
