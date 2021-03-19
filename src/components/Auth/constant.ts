75cd732dadaebdd6ec6134386c9ae6f662400f5c
Louvre Chow <louvrecly@gmail.com>
2021-03-19 17:53:39 +0800

    feat/auth_login: Create singupLinks constant

---
+++ src/components/Auth/constant.ts
@@ -0,0 +1,5 @@
+export const signupLinks = [
+  { key: 'email', text: 'Sign up with email' },
+  { key: 'facebook', text: 'Sign up with facebook' },
+  { key: 'google', text: 'Sign up with Google+' },
+];
